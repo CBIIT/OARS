95cdc119b7fd044dacd8c90ebf20a870e0aef9df
Jonathan Theismann <jtheismann@innovativesol.com>
2022-04-28 14:35:15 -0400

    Remove sample code

--- Program.cs
+++ Program.cs
@@ -16,7 +16,6 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-builder.Services.AddSingleton<WeatherForecastService>();
 builder.Services.AddSingleton<AadService>();
 builder.Services.AddSingleton<PbiEmbedService>();
 builder.Services.AddScoped<PbiInterop>();
