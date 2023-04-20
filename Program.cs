3ae9b49ee2c0e9d74f47a6e205e5eaaa71863e9e
Julia McDonald <jmcdonald@innovativesol.com>
2023-04-20 10:34:34 -0400

    foundational UI, removed cognito auth

--- Program.cs
+++ Program.cs
@@ -10,6 +10,10 @@ using System.Text.Json;
 using TheradexPortal.Data;
 using TheradexPortal.Data.PowerBI;
 using TheradexPortal.Data.PowerBI.Models;
+using Blazorise;
+using Blazorise.Tailwind;
+using Blazorise.Icons.FontAwesome;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -20,36 +24,41 @@ builder.Services.AddSingleton<AadService>();
 builder.Services.AddSingleton<PbiEmbedService>();
 builder.Services.AddScoped<PbiInterop>();
 
+// Add Blazorise and Tailwind UI
+builder.Services.AddBlazorise();
+builder.Services.AddTailwindProviders();
+builder.Services.AddFontAwesomeIcons();
+
 // Loading appsettings.json in C# Model classes
 builder.Services.Configure<PowerBI>(builder.Configuration.GetSection("PowerBI"));
 builder.Services.Configure<AzureAd>(builder.Configuration.GetSection("PowerBICredentials"));
 
 // Configure Cognito auth
-var sessionCookieLifetime = builder.Configuration.GetValue("SessionCookieLifetimeMinutes", 60);
-builder.Services.AddAuthentication(options =>
-{
-    options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
-    options.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
-})
-.AddCookie(options => {
-    options.ExpireTimeSpan = TimeSpan.FromMinutes(sessionCookieLifetime);
-})
-.AddOpenIdConnect(options =>
-{
-    options.SignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+//var sessionCookieLifetime = builder.Configuration.GetValue("SessionCookieLifetimeMinutes", 60);
+//builder.Services.AddAuthentication(options =>
+//{
+//    options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+//    options.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
+//})
+//.AddCookie(options => {
+//    options.ExpireTimeSpan = TimeSpan.FromMinutes(sessionCookieLifetime);
+//})
+//.AddOpenIdConnect(options =>
+//{
+//    options.SignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
 
-    options.Authority = builder.Configuration.GetValue<string>("CognitoConfig:Authority");
-    options.ClientId = builder.Configuration.GetValue<string>("CognitoConfig:ClientId");
-    options.ClientSecret = builder.Configuration.GetValue<string>("CognitoConfig:ClientSecret");
+//    options.Authority = builder.Configuration.GetValue<string>("CognitoConfig:Authority");
+//    options.ClientId = builder.Configuration.GetValue<string>("CognitoConfig:ClientId");
+//    options.ClientSecret = builder.Configuration.GetValue<string>("CognitoConfig:ClientSecret");
 
-    options.ResponseType = OpenIdConnectResponseType.Code;
-    options.SaveTokens = false;
-    options.GetClaimsFromUserInfoEndpoint = true;
-    options.Scope.Add("openid");
-    options.Scope.Add("profile");
-    options.Scope.Add("email");
+//    options.ResponseType = OpenIdConnectResponseType.Code;
+//    options.SaveTokens = false;
+//    options.GetClaimsFromUserInfoEndpoint = true;
+//    options.Scope.Add("openid");
+//    options.Scope.Add("profile");
+//    options.Scope.Add("email");
 
-});
+//});
 
 builder.Services.AddHttpContextAccessor();
 
@@ -72,8 +81,8 @@ if (!app.Environment.IsDevelopment())
 app.UseStaticFiles();
 app.UseRouting();
 
-app.UseAuthentication();
-app.UseAuthorization();
+//app.UseAuthentication();
+//app.UseAuthorization();
 
 app.MapBlazorHub();
 app.MapFallbackToPage("/_Host");
