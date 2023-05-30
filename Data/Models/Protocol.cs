71e455968d24880160b60ca7aada002b6125ba67
Julia McDonald <jmcdonald@innovativesol.com>
2023-05-30 10:31:46 -0400

    added overview and filtering

--- Data/Models/Protocol.cs
+++ Data/Models/Protocol.cs
@@ -7,8 +7,8 @@ namespace TheradexPortal.Data.Models
     [Keyless]
     public class Protocol
     {
-        //not unique - can have same study id for different statuses
-        public string? StudyId { get; set; } 
+        //unique index
+        public string? Study_Id { get; set; } 
 
         public string? Institution { get; set; }
 
@@ -54,19 +54,19 @@ namespace TheradexPortal.Data.Models
 
         public string? Informed_Consent { get; set; }
 
-        public DateTime? IRB_Approval { get; set; }
+        public DateTime? IRB_Approval_DT { get; set; }
 
-        public DateTime? NCI_Approval_Date { get; set; }
+        public DateTime? NCI_Approval_DT { get; set; }
 
-        public DateTime? Activation_Date { get; set; }
+        public DateTime? Activation_DT { get; set; }
         
-        public DateTime? Closed_To_Accrual { get; set; }
+        public DateTime? Closed_To_Accrual_DT { get; set; }
 
-        public DateTime? All_Pts_Off_Study { get; set; }
+        public DateTime? All_Pts_Off_Study_DT { get; set; }
 
-        public DateTime? Completed { get; set; }
+        public DateTime? Completed_DT { get; set; }
 
-        public DateTime? Archived { get; set; }
+        public DateTime? Archived_DT { get; set; }
 
         public string? Disease_1 { get; set; }
 
@@ -88,8 +88,8 @@ namespace TheradexPortal.Data.Models
 
         public string? Current_Status { get; set; }
         
-        // determines which record is most recent for a given study id
-        public DateTime Effective_Date { get; set; }
+        // what does this mean now that entries are unique per study?
+        public DateTime Effective_DT { get; set; }
 
         //primary key, unique
         public string? PR_Record_Id { get; set; } 
