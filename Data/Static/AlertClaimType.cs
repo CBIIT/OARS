aa55b87f7df6ee49e7d960c30f9dba775c57659c
djeselnik223108 <djeselnik@theradex.com>
2023-08-17 12:06:40 -0400

    Fixed Dashboard Alerts Bar, adjusted to new needs
    -Fixed EventCallback error, Dashboards now properly pass from PrimaryNav -> PrimaryNavLayout, where an event calls and updates the Dashboard Alerts.
    -Dashboard Notes are hidden
    -Staging for Alert Claims

---
+++ Data/Static/AlertClaimType.cs
@@ -0,0 +1,9 @@
+﻿namespace TheradexPortal.Data.Static
+{
+    public sealed class AlertClaimType
+    {
+        static public string AlertIdsViewed = "AlertIdsViewed";
+        static public string AlertIdsDismissed = "AlertIdsDismissed";
+        static public string CurrentDashboardId = "CurrentDashboardId";
+    }
+}
