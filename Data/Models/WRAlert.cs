6c97df9de78f763f5d9d100a785610cbd528da3b
djeselnik223108 <djeselnik@theradex.com>
2023-08-24 13:52:53 -0400

    Alert Styling fixes and polishing
    - System alerts now are sized and overflow properly
    - Removed Bar from Dashboard alerts
    - Alert Session Data clears on landing page, AKA at login and logout
    - Redefined colors for alerts
    - Removing Role column from WRAlert

--- Data/Models/WRAlert.cs
+++ Data/Models/WRAlert.cs
@@ -17,6 +17,5 @@ namespace TheradexPortal.Data.Models
         public DateTime? UpdateDate { get; set; }
         public DateTime? StartDate { get; set; }
         public DateTime? EndDate { get; set; }
-        public string? Role { get; set; }
     }
 }
