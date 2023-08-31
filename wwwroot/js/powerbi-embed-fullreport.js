aaca1b88252f2d64c0342ebc53836bd0001cfaa5
wfarbman <wfarbman@theradex.com>
2023-08-31 12:10:39 -0400

    Login page updates & report studyid filter

--- wwwroot/js/powerbi-embed-fullreport.js
+++ wwwroot/js/powerbi-embed-fullreport.js
@@ -1,50 +1,45 @@
 const models = window['powerbi-client'].models;
 
 function callDotNetSaveStudyMethod(study) {
-    DotNet.invokeMethodAsync("TheradexPortal", "SaveStudyFromSlicer", study[0])
+    DotNet.invokeMethodAsync("TheradexPortal", "SaveStudyFromSlicer", study)
         .then(data => {
             // once async call is done, this is to display the result returned by .NET call
             console.log("Slicer data :" + data);
         });
 }
 
-function embedFullReport(reportContainer, accessToken, filterTargets, embedUrl, embedReportId, studyFilter, slicerSetStudy, studySlicer) {
+function embedFullReport(reportContainer, accessToken, filterTargets, embedUrl, embedReportId, studyFilter, startingStudy) {
 
-    // studyFilter = list of studies to show in slicer
-    // slicerSetStudy = current study to default the slicer to
-    // studySlicer = power bi guid of the study id slicer
+    // studyFilter = list of studies to show in study id filter
+    // startingStudy = the study that should be checked to start
 
     const study_filter =
     {
-        $schema: "http://powerbi.com/product/schema#advanced",
+        $schema: "http://powerbi.com/product/schema#basic",
         target: {
             table: "PROTOCOL",
             column: "STUDY_ID"
         },
         operator: "In",
         values: studyFilter,
-        filterType: models.FilterType.AdvancedFilter,
-        requireSingleSelection: false,
+        filterType: models.FilterType.BasicFilter,
+        requireSingleSelection: true,
         displaySettings: { isHiddenInViewMode: true }
     }
 
-    const slicer_filter =
+    const study_filter2 =
     {
-        selector: new models.VisualSelector(studySlicer),
-        state: {
-            filters: [
-                {
-                    "$schema": "http://powerbi.com/product/schema#basic",
-                    "target": {
-                        "table": "PROTOCOL",
-                        "column": "STUDY_ID"
-                    },
-                    "operator": "In",
-                    "values": [slicerSetStudy]
-                }
-            ]
-        }
-    };
+        $schema: "http://powerbi.com/product/schema#basic",
+        target: {
+            table: "PROTOCOL",
+            column: "STUDY_ID"
+        },
+        operator: "In",
+        values: [startingStudy],
+        filterType: models.FilterType.BasicFilter,
+        requireSingleSelection: true,
+        displaySettings: { isHiddenInViewMode: false }
+    }
 
     // Build config
     var config = {
@@ -55,42 +50,46 @@ function embedFullReport(reportContainer, accessToken, filterTargets, embedUrl, 
         id: embedReportId,
         permissions: models.Permissions.All,
         background: models.BackgroundType.Transparent,
-        slicers: [slicer_filter], 
         settings: {
             filterPaneEnabled: true,
             navContentPaneEnabled: false,
             background: models.BackgroundType.Transparent
         },
-        filters: [study_filter]
+        filters: [study_filter, study_filter2]
     };
 
     // Embed report
     let report = powerbi.embed(reportContainer, config);
 
-    console.log(studyFilter);
+    let curStudies = "";
+    let newStudies = "";
+    let filterCount = 0;
+    let i = 0;
 
-    report.on('dataSelected', async function () {
-        //console.log("Visual clicked event triggred");
+    report.on('rendered', async function () {
+        console.log('rendered event');
+        const filters = await report.getFilters();
+        console.log("Report Filters: " + filters);
+        console.log("# of filters: " + filters.length);
+        filterCount = filters.length;
 
-        // Get pages
-        const pages = await report.getPages();
-
-        // Retrieve the page that contain the slicer. For the sample report it will be the active page
-        let pageWithSlicer = pages.filter(function (page) {
-            return page.isActive;
-        })[0];
-
-        // Get visuals present on the active page
-        const visuals = await pageWithSlicer.getVisuals();
-
-        // Retrieve the target visual.
-        let slicer = visuals.filter(function (visual) {
-            return visual.type === "slicer" && visual.name === studySlicer;
-        })[0];
-
-        // Get the slicer state
-        const state = await slicer.getSlicerState();
-        callDotNetSaveStudyMethod(state.filters[0].values);
+        newStudies = "";
+        console.log(filters[filterCount-1].values);
+        for (i in filters[filterCount - 1].values) {
+            if (newStudies == "") {
+                newStudies = filters[filterCount - 1].values[i]
+            }
+            else {
+                newStudies = newStudies + "," + filters[filterCount - 1].values[i];
+            }
+        }
+        console.log(newStudies);
+        if (newStudies != curStudies) {
+            console.log("Saving studies");
+            console.log("Curr:" + curStudies + "   New: " + newStudies);
+            curStudies = newStudies;
+            await callDotNetSaveStudyMethod(newStudies);
+        }
     });
 
     window.fullReport = report;
