6711fcd8e8d907283cb2ffc7b73e5cc8fbb1c1f1
djeselnik223108 <djeselnik@theradex.com>
2023-09-06 15:15:26 -0400

    Alerts now use strictly date, end date inclusive
    - Alerts check purely on day for valid start and end date, end date is inclusive.
    - Adjusted landing accordion scrollbar to the collapseable content and made the max height smaller

--- Data/Services/AlertService.cs
+++ Data/Services/AlertService.cs
@@ -27,56 +27,56 @@ namespace TheradexPortal.Data.Services
         public async Task<IList<WRAlert>> GetAllActiveAlertsAsync()
         {
             return await context.Alerts.Where(a => a.IsActive == active &&
-            (dateTime >= a.StartDate && (dateTime <= a.EndDate || a.EndDate.Equals(null))) &&
+            (dateTime >= a.StartDate!.Value.Date && (dateTime <= a.EndDate!.Value.Date.AddDays(1) || a.EndDate.Equals(null))) &&
             a.AlertType == alertType).ToListAsync();
         }
 
         public async Task<IList<WRAlert>> GetAllActiveNotesAsync()
         {
             return await context.Alerts.Where(a => a.IsActive == active &&
-            (dateTime >= a.StartDate && (dateTime <= a.EndDate || a.EndDate.Equals(null))) &&
+            (dateTime >= a.StartDate!.Value.Date && (dateTime <= a.EndDate!.Value.Date.AddDays(1) || a.EndDate.Equals(null))) &&
             a.AlertType == noteType).ToListAsync();
         }
 
         public async Task<IList<WRAlert>> GetActiveSystemAlertsAsync()
         {
             return await context.Alerts.Where(a => a.IsActive == active &&
-            (dateTime >= a.StartDate && (dateTime <= a.EndDate || a.EndDate.Equals(null))) &&
+            (dateTime >= a.StartDate!.Value.Date && (dateTime <= a.EndDate!.Value.Date.AddDays(1) || a.EndDate.Equals(null))) &&
             a.AlertType == alertType && a.PageName == system).OrderByDescending(a => a.WRAlertId).ToListAsync();
         }
 
         public async Task<IList<WRAlert>> GetActiveSystemNotesAsync()
         {
             return await context.Alerts.Where(a => a.IsActive == active &&
-            (dateTime >= a.StartDate && (dateTime <= a.EndDate || a.EndDate.Equals(null))) &&
+            (dateTime >= a.StartDate!.Value.Date && (dateTime <= a.EndDate!.Value.Date.AddDays(1) || a.EndDate.Equals(null))) &&
             a.AlertType == noteType && a.PageName == system).OrderByDescending(a => a.WRAlertId).ToListAsync();
         }
 
         public async Task<IList<WRAlert>> GetActiveLoginAlertsAsync()
         {
             return await context.Alerts.Where(a => a.IsActive == active && 
-            (dateTime >= a.StartDate && (dateTime <= a.EndDate || a.EndDate.Equals(null))) && 
+            (dateTime >= a.StartDate!.Value.Date && (dateTime <= a.EndDate!.Value.Date.AddDays(1) || a.EndDate.Equals(null))) && 
             a.AlertType == alertType && a.PageName == loginPage).OrderByDescending(a => a.WRAlertId).ToListAsync();
         }
         
         public async Task<IList<WRAlert>> GetActiveLoginNotesAsync()
         {
             return await context.Alerts.Where(a => a.IsActive == active &&
-            (dateTime >= a.StartDate && (dateTime <= a.EndDate || a.EndDate.Equals(null))) &&  
+            (dateTime >= a.StartDate!.Value.Date && (dateTime <= a.EndDate!.Value.Date.AddDays(1) || a.EndDate.Equals(null))) &&  
             a.AlertType == noteType && a.PageName == loginPage).OrderByDescending(a => a.WRAlertId).ToListAsync();
         }
         
         public async Task<IList<WRAlert>> GetActiveDashboardAlertsByIdAsync(int dashboardId)
         {
             return await context.Alerts.Where(a => a.IsActive == active &&
-            (dateTime >= a.StartDate && (dateTime <= a.EndDate || a.EndDate.Equals(null))) &&
+            (dateTime >= a.StartDate!.Value.Date && (dateTime <= a.EndDate!.Value.Date.AddDays(1) || a.EndDate.Equals(null))) &&
             a.AlertType == alertType && a.DashboardId == dashboardId).OrderByDescending(a => a.WRAlertId).ToListAsync();
         }
 
         public async Task<IList<WRAlert>> GetActiveDashboardNotesByIdAsync(int dashboardId)
         {
             return await context.Alerts.Where(a => a.IsActive == active &&
-            (dateTime >= a.StartDate && (dateTime <= a.EndDate || a.EndDate.Equals(null))) &&
+            (dateTime >= a.StartDate!.Value.Date && (dateTime <= a.EndDate!.Value.Date.AddDays(1) || a.EndDate.Equals(null))) &&
             a.AlertType == noteType && a.DashboardId == dashboardId).OrderByDescending(a => a.WRAlertId).ToListAsync();
         }
     }
