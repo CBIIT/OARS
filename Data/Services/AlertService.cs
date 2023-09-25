4e3479068539dc0865aca24f60e389df70d3d9a0
djeselnik223108 <djeselnik@theradex.com>
2023-09-25 15:32:46 -0400

    Alert Admin changes, intermediate commit before moving alert variables to Service

--- Data/Services/AlertService.cs
+++ Data/Services/AlertService.cs
@@ -7,11 +7,13 @@ namespace TheradexPortal.Data.Services
     public class AlertService : BaseService, IAlertService
     {
         private readonly bool active = true;
-        private readonly string noteType = "Note";
-        private readonly string alertType = "Alert";
-        private readonly string system = "System";
-        private readonly string loginPage = "Login";
         private readonly DateTime dateTime = DateTime.Now;
+
+        public readonly string NoteType = "Note";
+        public readonly string AlertType = "Alert";
+        public readonly string SystemPage = "System";
+        public readonly string LoginPage = "Login";
+        public readonly string DashboardPage = "Dashboard";
 
         public AlertService(IDbContextFactory<WrDbContext> dbFactory) : base(dbFactory) { }
 
@@ -22,67 +24,67 @@ namespace TheradexPortal.Data.Services
 
         public async Task<IList<WRAlert>> GetAllAlertsAsync()
         {
-            return await context.Alerts.Where(a => a.AlertType == alertType).ToListAsync();
+            return await context.Alerts.Where(a => a.AlertType == AlertType).ToListAsync();
         }
         public async Task<IList<WRAlert>> GetAllNotesAsync()
         {
-            return await context.Alerts.Where(a => a.AlertType == noteType).ToListAsync();
+            return await context.Alerts.Where(a => a.AlertType == NoteType).ToListAsync();
         }
 
         public async Task<IList<WRAlert>> GetAllActiveAlertsAsync()
         {
             return await context.Alerts.Where(a => a.IsActive == active &&
             (dateTime >= a.StartDate!.Value.Date && (dateTime <= a.EndDate!.Value.Date.AddDays(1) || a.EndDate.Equals(null))) &&
-            a.AlertType == alertType).ToListAsync();
+            a.AlertType == AlertType).ToListAsync();
         }
 
         public async Task<IList<WRAlert>> GetAllActiveNotesAsync()
         {
             return await context.Alerts.Where(a => a.IsActive == active &&
             (dateTime >= a.StartDate!.Value.Date && (dateTime <= a.EndDate!.Value.Date.AddDays(1) || a.EndDate.Equals(null))) &&
-            a.AlertType == noteType).ToListAsync();
+            a.AlertType == NoteType).ToListAsync();
         }
 
         public async Task<IList<WRAlert>> GetActiveSystemAlertsAsync()
         {
             return await context.Alerts.Where(a => a.IsActive == active &&
             (dateTime >= a.StartDate!.Value.Date && (dateTime <= a.EndDate!.Value.Date.AddDays(1) || a.EndDate.Equals(null))) &&
-            a.AlertType == alertType && a.PageName == system).OrderByDescending(a => a.WRAlertId).ToListAsync();
+            a.AlertType == AlertType && a.PageName == SystemPage).OrderByDescending(a => a.WRAlertId).ToListAsync();
         }
 
         public async Task<IList<WRAlert>> GetActiveSystemNotesAsync()
         {
             return await context.Alerts.Where(a => a.IsActive == active &&
             (dateTime >= a.StartDate!.Value.Date && (dateTime <= a.EndDate!.Value.Date.AddDays(1) || a.EndDate.Equals(null))) &&
-            a.AlertType == noteType && a.PageName == system).OrderByDescending(a => a.WRAlertId).ToListAsync();
+            a.AlertType == NoteType && a.PageName == SystemPage).OrderByDescending(a => a.WRAlertId).ToListAsync();
         }
 
         public async Task<IList<WRAlert>> GetActiveLoginAlertsAsync()
         {
             return await context.Alerts.Where(a => a.IsActive == active && 
             (dateTime >= a.StartDate!.Value.Date && (dateTime <= a.EndDate!.Value.Date.AddDays(1) || a.EndDate.Equals(null))) && 
-            a.AlertType == alertType && a.PageName == loginPage).OrderByDescending(a => a.WRAlertId).ToListAsync();
+            a.AlertType == AlertType && a.PageName == LoginPage).OrderByDescending(a => a.WRAlertId).ToListAsync();
         }
         
         public async Task<IList<WRAlert>> GetActiveLoginNotesAsync()
         {
             return await context.Alerts.Where(a => a.IsActive == active &&
             (dateTime >= a.StartDate!.Value.Date && (dateTime <= a.EndDate!.Value.Date.AddDays(1) || a.EndDate.Equals(null))) &&  
-            a.AlertType == noteType && a.PageName == loginPage).OrderByDescending(a => a.WRAlertId).ToListAsync();
+            a.AlertType == NoteType && a.PageName == LoginPage).OrderByDescending(a => a.WRAlertId).ToListAsync();
         }
         
         public async Task<IList<WRAlert>> GetActiveDashboardAlertsByIdAsync(int dashboardId)
         {
             return await context.Alerts.Where(a => a.IsActive == active &&
             (dateTime >= a.StartDate!.Value.Date && (dateTime <= a.EndDate!.Value.Date.AddDays(1) || a.EndDate.Equals(null))) &&
-            a.AlertType == alertType && a.DashboardId == dashboardId).OrderByDescending(a => a.WRAlertId).ToListAsync();
+            a.AlertType == AlertType && a.DashboardId == dashboardId).OrderByDescending(a => a.WRAlertId).ToListAsync();
         }
 
         public async Task<IList<WRAlert>> GetActiveDashboardNotesByIdAsync(int dashboardId)
         {
             return await context.Alerts.Where(a => a.IsActive == active &&
             (dateTime >= a.StartDate!.Value.Date && (dateTime <= a.EndDate!.Value.Date.AddDays(1) || a.EndDate.Equals(null))) &&
-            a.AlertType == noteType && a.DashboardId == dashboardId).OrderByDescending(a => a.WRAlertId).ToListAsync();
+            a.AlertType == NoteType && a.DashboardId == dashboardId).OrderByDescending(a => a.WRAlertId).ToListAsync();
         }
     }
 }
