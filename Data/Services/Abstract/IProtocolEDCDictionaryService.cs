3cbcf5c179290ad9788cb242860a8ce6bc57d34f
JT Fitzgerald <jfitzgerald@innovativesol.com>
2024-03-21 12:51:56 -0400

    TDMU-38 - initial setup for protocol edc dictionary edit

--- Data/Services/Abstract/IProtocolEDCDictionaryService.cs
+++ Data/Services/Abstract/IProtocolEDCDictionaryService.cs
@@ -5,6 +5,9 @@ namespace TheradexPortal.Data.Services.Abstract
 {
     public interface IProtocolEDCDictionaryService
     {
+        public Task<List<ProtocolEDCDictionary>> GetDictionariesByMappingId(int mappingId);
+        public Task<bool> SaveDictionary(ProtocolEDCDictionary dictionary, int mappingId);
+        public Task<bool> DeleteDictionary(ProtocolEDCDictionary dictionary);
         public Task<bool> BulkSaveDictionaries(DataTable dictionaries);
         public Task<bool> DeleteAllDictionariesForMappingId(int mappingId);
     }
