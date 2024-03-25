07b180786c33b5e4d4668173134f106ea8230252
JT Fitzgerald <jfitzgerald@innovativesol.com>
2024-03-25 14:17:51 -0400

    Protocol Agent edit, confirmation dialog for delete

---
+++ Data/Models/ProtocolAgent.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TheradexPortal.Data.Models
+{
+    [Table("ProtocolAgent", Schema = "DMU")]
+    public class ProtocolAgent
+    {
+        [Key, Column("Protocol_Agent_Id")]
+        public int ProtocolAgentId { get; set; }
+        [Column("Protocol_Mapping_Id")]
+        public int ProtocolMappingId { get; set; }
+        [Column("Nsc_Number")]
+        public string NscNumber { get; set; }
+        [Column("Agent_Name")]
+        public string AgentName { get; set; }
+        [Column("Create_Date")]
+        public DateTime CreateDate { get; set; }
+        [Column("Update_Date")]
+        public DateTime UpdateDate { get; set; }
+    }
+}
---
+++ Data/Services/ProtocolAgentService.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TheradexPortal.Data.Models;
+using TheradexPortal.Data.Services.Abstract;
+
+namespace TheradexPortal.Data.Services
+{
+    public class ProtocolAgentService: BaseService, IProtocolAgentService
+    {
+        private readonly IErrorLogService _errorLogService;
+        public ProtocolAgentService(IDbContextFactory<ThorDBContext> context, IErrorLogService errorLogService) : base(context)
+        {
+            _errorLogService = errorLogService;
+        }
+
+        public async Task<List<ProtocolAgent>> GetAgentsByMappingId(int mappingId)
+        {
+            return await context.ProtocolAgents.Where(x => x.ProtocolMappingId == mappingId).ToListAsync();
+        }
+
+        public async Task<bool> SaveAgent(ProtocolAgent agent, int mappingId)
+        {
+            try
+            {
+                ProtocolAgent currAgent = context.ProtocolAgents.FirstOrDefault(x => x.ProtocolAgentId == agent.ProtocolAgentId);
+                if (currAgent == null || agent.CreateDate == null)
+                {
+                    agent.ProtocolMappingId = mappingId;
+                    agent.CreateDate = DateTime.Now;
+                    agent.UpdateDate = DateTime.Now;
+                    context.ProtocolAgents.Add(agent);
+                }
+                else
+                {
+                    currAgent.UpdateDate = DateTime.Now;
+                    currAgent.ProtocolMappingId = mappingId;
+                    currAgent.NscNumber = agent.NscNumber;
+                    currAgent.AgentName = agent.AgentName;
+
+                    context.ProtocolAgents.Update(agent);
+                }
+                await context.SaveChangesAsync();
+                return true;
+            } catch (Exception ex)
+            {
+                await _errorLogService.SaveErrorLogAsync(0, "", ex.InnerException, ex.Source, ex.Message, ex.StackTrace);
+                return false;
+            }
+
+        }
+
+        public async Task<bool> DeleteAgent(ProtocolAgent agent)
+        {
+            try
+            {
+                context.ProtocolAgents.Remove(agent);
+                await context.SaveChangesAsync();
+                return true;
+            } catch (Exception ex)
+            {
+                await _errorLogService.SaveErrorLogAsync(0, "", ex.InnerException, ex.Source, ex.Message, ex.StackTrace);
+                return false;
+            }
+        }
+    }
+}
