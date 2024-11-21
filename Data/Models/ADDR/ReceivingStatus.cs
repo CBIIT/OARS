3eb7f6f9db814ba84a1ca723095a350715dcd400
Manish Rathi <mgrathi.99@gmail.com>
2024-11-21 09:17:01 -0800

    Mr/addr receiving status adding deep link (#255)
    * feat: Adding Deep link to ADDR Dummy Data
    
    * fix: JSON deserialization failure
    
    * fix: Row Styling
    
    * fix: Row Styling
    
    * fix: Column Names
    
    * fix: CarrierTrackingNo  to SubmissionCarrierTrackingNo
    
    ---------
    
    Co-authored-by: ManishRathi <manishrathi@ManishMacbookPro-2.local>

--- Data/Models/ADDR/ReceivingStatus.cs
+++ Data/Models/ADDR/ReceivingStatus.cs
@@ -6,37 +6,6 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace TheradexPortal.Data.Models.ADDR
 {
-    public class ReceivingStatus2 : ADDRBase
-    {
-        [DynamoDBProperty]
-        public string? CarrierTrackingNo { get; set; }
-
-        [DynamoDBProperty]
-        public string? ShippedDate { get; set; }
-
-        [DynamoDBProperty]
-        public string? CarrierName { get; set; }
-        [DynamoDBProperty]
-        public string? PackagingCondition { get; set; }
-        [DynamoDBProperty]
-        public string? ReceivingSite { get; set; }
-        [DynamoDBProperty]
-        public string? ReceivedDateTime { get; set; }
-        [DynamoDBProperty]
-        public string? PackagingDetailCondition { get; set; }
-        [DynamoDBProperty]
-        public string? Inadreas { get; set; }
-        [DynamoDBProperty]
-        public string? Notes { get; set; }
-
-        [DynamoDBProperty]
-        public string? VariCount { get; set; }
-
-        [DynamoDBProperty]
-        public string? RaveCount { get; set; }
-
-    }
-
     public class ReceivingStatus : ADDRBase
     {
         [DynamoDBProperty]
