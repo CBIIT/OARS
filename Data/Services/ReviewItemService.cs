220bd4271d05b2c5a524e7b329f4c025d06f45d4
Mike Worster <mworster@theradex.com>
2024-12-03 13:06:47 -0500

    Updates for styling, fixing up MO page, fixing up the admin page for review items

--- Data/Services/ReviewItemService.cs
+++ Data/Services/ReviewItemService.cs
@@ -14,6 +14,22 @@ namespace TheradexPortal.Data.Services
             _errorLogService = errorLogService;
             _navManager = navigationManager;
         }
+
+        public async Task<IList<ReviewItem>> GetReviewItemListAsync(string? reviewType = "", char? isActive = null)
+        {
+            var query = context.Set<ReviewItem>().AsQueryable();
+            if (!string.IsNullOrEmpty(reviewType))
+            {
+                query = query.Where(item =>  item.ReviewType == reviewType);
+            }
+            if(isActive.HasValue)
+            {
+                query = query.Where(item => item.IsActive == isActive.Value);
+            }
+
+            return await query.ToListAsync();
+        }
+
         public async Task<IList<ReviewItem>> GetActiveReviewItemsAsync()
         {
             return await context.ReviewItems.Where(p=>p.IsActive == 'T' && p.ReviewType == "PI").ToListAsync();
@@ -23,5 +39,35 @@ namespace TheradexPortal.Data.Services
         {
             return await context.ReviewItems.Where(p => p.ReviewType == reviewType).ToListAsync();
         }
+        public async Task<bool> DeactivateReviewItemsAsync(int reviewItemId)
+        {
+            var res =  await context.ReviewItems.Where(p => p.ReviewItemId == reviewItemId).FirstOrDefaultAsync();
+            res.IsActive = 'F';
+            context.SaveChanges();
+            return true;
+        }
+        public async Task<ReviewItem> GetReviewItemByIdAsync(int? reviewItemId)
+        {
+            return await context.ReviewItems.Where(p => p.ReviewItemId == reviewItemId).FirstOrDefaultAsync();
+        }
+
+        public async void SaveReviewItemAsync(ReviewItem item)
+        {
+            var itemToUpdate = await context.ReviewItems.FirstOrDefaultAsync(ri => ri.ReviewItemId == item.ReviewItemId);
+            if (itemToUpdate != null)
+            {
+                itemToUpdate.ReviewType = item.ReviewType;
+                itemToUpdate.IsActive = item.IsActive;
+                itemToUpdate.ReviewItemName = item.ReviewItemName;
+                itemToUpdate.UpdateDate = item.UpdateDate;
+            }
+            else
+            {
+                context.ReviewItems.Add(item);
+            }
+            await context.SaveChangesAsync();
+            return;
+        }
+
     }
 }
