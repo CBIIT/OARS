c83bfa5382f8a21695512229629a5258029b163d
mworster <mike.worster@gmail.com>
2025-01-13 09:03:49 -0500

    Add webreporting to model and default value (#332)
    Co-authored-by: Mike Worster <mworster@theradex.com>

--- Data/Models/ReviewHistory.cs
+++ Data/Models/ReviewHistory.cs
@@ -33,5 +33,7 @@ namespace TheradexPortal.Data.Models
         public int? ReviewId { get; set; }
         [Column("REVIEW_PERIOD_NAME")]
         public string? ReviewPeriodName { get; set; }
+        [Column("IS_WEBREPORTING")]
+        public char? IsWebReporting { get; set; }
     }
 }
