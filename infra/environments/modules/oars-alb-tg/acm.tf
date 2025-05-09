d473c6f8f4e536891fbb9ea6f1feb94f34051835
gtheradex <gideon.esomonu@nih.gov>
2025-05-09 13:43:43 -0400

    Comment out unused ACM certificate resource and update data source configuration

--- infra/environments/modules/oars-alb-tg/acm.tf
+++ infra/environments/modules/oars-alb-tg/acm.tf
@@ -1,9 +1,15 @@
-resource "aws_acm_certificate" "oars_cert" {
-  domain_name       = var.domain_name_cert
-  validation_method = "DNS"
-  lifecycle {
-    create_before_destroy = true
-  }
+# resource "aws_acm_certificate" "oars_cert" {
+#   domain_name       = var.domain_name_cert
+#   validation_method = "DNS"
+#   lifecycle {
+#     create_before_destroy = true
+#   }
+data "aws_acm_certificate" "oars_cert" {
+  domain      =  var.domain_name_cert  
+  statuses    = ["ISSUED"]     
+  types       = ["IMPORTED"]   
+  most_recent = true
+}
 }
 resource "aws_acm_certificate_validation" "oars_cert_validate" {
   certificate_arn = aws_acm_certificate.oars_cert.arn
