28e31e4b2d70604b9f695c88f469212fa9b7d558
gtheradex <gideon.esomonu@nih.gov>
2025-05-09 13:58:29 -0400

    Comment out unused ACM certificate validation and DNS record resources

--- infra/environments/modules/oars-alb-tg/route53.tf
+++ infra/environments/modules/oars-alb-tg/route53.tf
@@ -2,14 +2,14 @@ resource "aws_route53_zone" "oars_zone" {
   name = var.domain_name
 }
 
-resource "aws_route53_record" "oars_cert_dns" {
-  allow_overwrite = true
-  name =  tolist(aws_acm_certificate.oars_cert.domain_validation_options)[0].resource_record_name
-  records = [tolist(aws_acm_certificate.oars_cert.domain_validation_options)[0].resource_record_value]
-  type = tolist(aws_acm_certificate.oars_cert.domain_validation_options)[0].resource_record_type
-  zone_id = aws_route53_zone.oars_zone.zone_id
-  ttl = 60
-}
+# resource "aws_route53_record" "oars_cert_dns" {
+#   allow_overwrite = true
+#   name =  tolist(aws_acm_certificate.oars_cert.domain_validation_options)[0].resource_record_name
+#   records = [tolist(aws_acm_certificate.oars_cert.domain_validation_options)[0].resource_record_value]
+#   type = tolist(aws_acm_certificate.oars_cert.domain_validation_options)[0].resource_record_type
+#   zone_id = aws_route53_zone.oars_zone.zone_id
+#   ttl = 60
+# }
 
 resource "aws_route53_record" "oars_alb_dns" {
   name = var.domain_name
